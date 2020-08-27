e3a2d256ad69f1d8f31a1522fc63b52ebd433fe0
Gaetano Pessa <g.pessa@goldrepublic.com>
2020-08-28 00:29:06 +0200

    Updates

---
+++ precache-manifest.bede495576688785ee6e1e85e26f9028.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fd14dc9e59b4891b783dd847246de6",
+    "url": "/test-9/index.html"
+  },
+  {
+    "revision": "7a472e04816dd510e534",
+    "url": "/test-9/static/css/main.29bfe7a8.chunk.css"
+  },
+  {
+    "revision": "9e2e82d57c3bb50398fa",
+    "url": "/test-9/static/js/2.248ed7ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-9/static/js/2.248ed7ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a472e04816dd510e534",
+    "url": "/test-9/static/js/main.84abf220.chunk.js"
+  },
+  {
+    "revision": "52a90107f1d87530abf6",
+    "url": "/test-9/static/js/runtime-main.d426554f.js"
+  },
+  {
+    "revision": "0768d87dfdcfc1ce8f13a8330a38fcd0",
+    "url": "/test-9/static/media/icon-fail.0768d87d.svg"
+  },
+  {
+    "revision": "1d2f229e9b65f23a42f3795043370f93",
+    "url": "/test-9/static/media/icon-success.1d2f229e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0112b664890cac370cb9ddf5aa9e793c.js"
+  "/test-9/precache-manifest.bede495576688785ee6e1e85e26f9028.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-9/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
